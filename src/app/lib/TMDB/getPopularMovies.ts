1f87dee293143c081d18e7301554c8926ee593bf
J. Clark <justindanielclark@gmail.com>
2023-06-08 21:03:23 -0700

    + getPopularMovies API call

--- src/app/lib/TMDB/getPopularMovies.ts
+++ src/app/lib/TMDB/getPopularMovies.ts
@@ -1,13 +1,39 @@
-const searchURL = "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=";
+// Returns Top 100 Movies Per TMDB's Current API As An Array
+
+const search = new URL("https://api.themoviedb.org");
+search.pathname = "/3/trending/movie/week";
+search.searchParams.set("include_adult", "false");
+search.searchParams.set("language", "en-US");
+
 const options: RequestInit = {
   method: "GET",
-  headers: new Headers([
-    ["accept", "application/json"],
-    ["Authorization", process.env.TMDB_AUTH_KEY as string],
-  ]),
+  headers: {
+    accept: "application/json",
+    Authorization: process.env.TMDB_AUTH_KEY as string,
+  },
 };
-export default async function getPopularMovies() {
-  const searches = new Array(10).map((item, idx) => fetch(`${searchURL}${idx + 1}`, options));
-  const result = await Promise.all(searches);
-  return result;
+export default async function getPopularMovies(): Promise<Array<fetchedMovie>> {
+  const fetches: Array<Promise<Response>> = [];
+  for (let i = 1; i <= 5; i++) {
+    const newURL = new URL(search);
+    newURL.searchParams.set("page", i.toString());
+    fetches.push(fetch(newURL, options));
+  }
+  const fetchedData = await Promise.all(fetches);
+
+  const jsonData = (await Promise.all(
+    fetchedData.map((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return null;
+    })
+  )) as Array<fetchedPopularMovies | null>;
+
+  return jsonData.reduce((acc, cur) => {
+    if (cur && cur.results) {
+      cur.results.forEach((movie) => acc.push(movie));
+    }
+    return acc;
+  }, [] as Array<fetchedMovie>);
 }
