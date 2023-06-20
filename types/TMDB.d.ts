ed51f67900afdb561524dd95768d6e591fde4010
JClark <justindanielclark@gmail.com>
2023-06-20 11:45:30 -0700

    types for user, fetched recommendations

--- types/TMDB.d.ts
+++ types/TMDB.d.ts
@@ -166,3 +166,26 @@ type Recommendation = {
   total_pages: number;
   total_results: number;
 };
+
+type searchResults = {
+  page: number;
+  total_pages: number;
+  total_results: number;
+  results: Array<MovieSearchResult>;
+};
+type MovieSearchResult = {
+  adult: false;
+  backdrop_path?: string;
+  genre_ids: Array<number>;
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
--- types/types.d.ts
+++ types/types.d.ts
@@ -1,23 +1,29 @@
-import { ObjectId } from "mongodb"
+import { ObjectId } from "mongodb";
 
 type User = {
-    //Combination of email[provider] ~ Example: jclark@gmail.com[github]
-    lookup: string;
-    handle: string;
-    lists: {
-        seenMovies: Array<MovieIDs>;
-        toWatch: Array<MovieIDs>
-    }
-    friends: Array<ObjectId>;
-}
+  //Combination of email[provider] ~ Example: jclark@gmail.com[github]
+  lookup: string;
+  handle: string;
+  lists: Array<List>;
+  friends: Array<Friend>;
+};
+
+type List = {
+  name: string;
+  movies: Array<MovieIDs>;
+};
+type Friend = {
+  name: string;
+  _id: ObjectId;
+};
 
 type MovieIDs = {
-    _id: ObjectId,
-    TMDB_ID: number,
-}
+  _id: ObjectId;
+  TMDB_ID: number;
+};
 
 type MovieList = {
-    name: string,
-    createdOn: Date,
-    movies: Array<MovieIDs>
-}
+  name: string;
+  createdOn: Date;
+  movies: Array<MovieIDs>;
+};
