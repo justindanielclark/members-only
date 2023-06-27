69f729d13f36d1b1e40688194ac1396735924a8d
JClark <justindanielclark@gmail.com>
2023-06-27 14:47:40 -0700

    +Header Dropdown, Footer Attributions

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import TMDB from "../../../public/TMDB.svg";
+import GitHubIcon from "@/lib/sharedComponents/Icons/GithubIcon";
+import Image from "next/image";
 function Footer() {
-  return <footer className="h-12 bg-slate-900 border-t border-slate-600">FOOTER</footer>;
+  return (
+    <footer className="h-12 gap-6 bg-slate-900 border-t border-slate-600 flex flex-row items-center w-full justify-center">
+      <Link href={"https://www.themoviedb.org/?language=en-US"} target="_blank">
+        <span className="mr-4 hidden sm:inline">Powered By:</span>
+        <Image src={TMDB} height={30} alt="TMDB logo" className="inline" />
+      </Link>
+      <Link href={"https://www.github.com/justindanielclark"} target="_blank">
+        <span className="mr-4 hidden sm:inline">Created By:</span>
+        <GitHubIcon />
+      </Link>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,21 @@
 "use client";
+import { useState } from "react";
 import Image from "next/image";
 import { signOut, signIn, useSession } from "next-auth/react";
 import React from "react";
 import GoogleSVG from "../../../public/GoogleLogo.svg";
 import GithubSVG from "../../../public/GithubLogo.svg";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import HamburgerIcon from "@/lib/sharedComponents/Icons/HamburgerIcon";
+import GearIcon from "@/lib/sharedComponents/Icons/GearIcon";
+import FriendIcon from "@/lib/sharedComponents/Icons/FriendIcon";
+import SignOutIcon from "@/lib/sharedComponents/Icons/SignOutIcon";
+
+const menuAnimatingStates = ["open", "opening", "closed", "closing"] as const;
+type AnimationState = (typeof menuAnimatingStates)[number];
 
 function Header() {
+  const [menuAnimationState, setMenuAnimationState] = useState<AnimationState>("closed");
   const { data: session, status } = useSession();
 
   let content: JSX.Element;
@@ -15,19 +23,21 @@ function Header() {
     case "authenticated": {
       if (session) {
         content = (
-          <div className="flex flex-row gap-4">
-            <Image
-              className="rounded-full border-2 border-slate-600"
-              src={session.user.image as string}
-              alt="profile_photo"
-              width={40}
-              height={40}
-            />
-            <button onClick={() => signOut()}>Sign Out!</button>
+          <div
+            className="flex flex-row items-center justify-center w-12 h-12 cursor-pointer"
+            onClick={() => {
+              if (menuAnimationState === "open") {
+                setMenuAnimationState("closing");
+              } else if (menuAnimationState === "closed") {
+                setMenuAnimationState("opening");
+              }
+            }}
+          >
+            <HamburgerIcon />
           </div>
         );
       } else {
-        content = <div>There has been an error...</div>;
+        content = <div>There has been an error with session...</div>;
       }
 
       break;
@@ -69,12 +79,84 @@ function Header() {
     }
   }
   return (
-    <header className="bg-slate-900 border-b border-slate-600">
-      <div className="max-w-7xl mx-auto flex justify-between h-12 items-center">
-        <Link href={session ? "/dashboard" : "/"}>MovieBase</Link>
-        {content}
+    <header className="bg-slate-900 border-b border-slate-600 relative">
+      <div className="max-w-7xl mx-auto bg-slate-900 z-50 relative">
+        <div className="relative w-full flex flex-row bg-slate-900 justify-between h-12 items-center z-50 px-4">
+          <Link href={session ? "/dashboard" : "/"}>MovieBase</Link>
+          {content}
+        </div>
+        {menuAnimationState !== "closed" && status == "authenticated" ? (
+          <HeaderMenu state={menuAnimationState} setState={setMenuAnimationState} />
+        ) : undefined}
       </div>
     </header>
+  );
+}
+
+type HeaderMenuProps = {
+  state: AnimationState;
+  setState: React.Dispatch<React.SetStateAction<AnimationState>>;
+};
+
+function HeaderMenu({ state, setState }: HeaderMenuProps) {
+  const classes: Array<string> = (() => {
+    switch (state) {
+      case "open": {
+        return ["-bottom-0.5", "translate-y-full"];
+      }
+      case "opening": {
+        return ["bottom-0", "translate-y-0", "animate-headerMenuDown"];
+      }
+      case "closed": {
+        return ["bottom-0", "translate-y-0"];
+      }
+      case "closing": {
+        return ["-bottom-0.5", "translate-y-full", "animate-headerMenuUp"];
+      }
+      default: {
+        throw new Error("Unexpected Animation State");
+      }
+    }
+  })();
+  return (
+    <div
+      className={`absolute right-5 bg-slate-900 rounded-b-lg ${classes.join(" ")}`}
+      style={{ animationFillMode: "forwards" }}
+      onAnimationEnd={(e) => {
+        if (e.animationName === "headerMenuDown") {
+          setState("open");
+        } else if (e.animationName === "headerMenuUp") {
+          setState("closed");
+        }
+      }}
+    >
+      <ul className="flex flex-col min-w-56">
+        <li className="text-right whitespace-nowrap p-2 cursor-pointer hover:bg-slate-700/80">
+          <Link href={"/profile"} className="w-full flex flex-row items-center gap-4 justify-end">
+            <span>Profile / Settings</span>
+            <GearIcon />
+          </Link>
+        </li>
+        <li className="text-right whitespace-nowrap p-2 cursor-pointer hover:bg-slate-700/80">
+          <Link href={"/friends"} className="w-full flex flex-row items-center gap-4 justify-end">
+            <span>View and Add Friends</span>
+            <FriendIcon />
+          </Link>
+        </li>
+        <li className="text-right whitespace-nowrap p-2 cursor-pointer hover:bg-slate-700/80">
+          <button
+            className="w-full text-right flex flex-row items-center gap-4 justify-end"
+            onClick={(e) => {
+              e.preventDefault();
+              signOut();
+            }}
+          >
+            <span>Sign Out</span>
+            <SignOutIcon />
+          </button>
+        </li>
+      </ul>
+    </div>
   );
 }
 
