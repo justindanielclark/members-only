a64ab41c3e5438fe0ca019b13f0f0e01f5a148dc
JClark <justindanielclark@gmail.com>
2023-07-12 21:33:35 -0700

    + padding and BG for subheaders for clarity

--- src/lib/sharedComponents/Headers/SubHeader.tsx
+++ src/lib/sharedComponents/Headers/SubHeader.tsx
@@ -2,5 +2,5 @@ type Props = {
   children: React.ReactNode;
 };
 export default function SubHeader({ children }: Props) {
-  return <h1 className="font-bold text-2xl">{children}</h1>;
+  return <h1 className="font-bold text-2xl bg-slate-900 p-4">{children}</h1>;
 }
